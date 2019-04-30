f314b6e8bbe17000e8bb40069aa48f01edb05ef4
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-04-30 14:34:21 +0200

    Fixed Semicolon

--- src/tuhhsdk/SharedObject.cpp
+++ src/tuhhsdk/SharedObject.cpp
@@ -12,7 +12,7 @@ SharedObject::SharedObject(const std::string& name, ThreadData& tData)
   : thread_()
   , threadDatum_(tData)
 {
-  Log(LogLevel::DEBUG) << "New SharedObject was instantiated with name: " << name
+  Log(LogLevel::DEBUG) << "New SharedObject was instantiated with name: " << name;
   ThreadFactoryBase* factory;
   for (factory = ThreadFactoryBase::begin; factory != nullptr; factory = factory->next)
   {
