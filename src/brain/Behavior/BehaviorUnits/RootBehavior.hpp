0fd79845fc26a30f6e5610ffc944c31c70dec52a
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-06-26 16:19:13 +0200

    Temporarily changed RootBehaviour to play back Audio

--- src/brain/Behavior/BehaviorUnits/RootBehavior.hpp
+++ src/brain/Behavior/BehaviorUnits/RootBehavior.hpp
@@ -3,8 +3,18 @@
 
 ActionCommand rootBehavior(const DataSet& d)
 {
+  //return ActionCommand::stand().combineAudio(ActionCommand::Audio::test());
+
+
   // If the NAO does not have foot contact it overrides the left LED with pink.
   const bool high = !d.bodyPose.footContact;
+
+  if(high){
+    return ActionCommand::stand().combineAudio(ActionCommand::Audio::test());
+  }else{
+    return ActionCommand::stand().combineAudio(ActionCommand::Audio::test2());
+  }
+
   if (d.gameControllerState.penalty == Penalty::NONE)
   {
     return high ? notPenalized(d).combineLeftLED(ActionCommand::LED::pink()) : notPenalized(d);
