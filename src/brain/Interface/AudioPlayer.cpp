7a62bdcd3dc4f94d6fff920f2ff96d3efecc5096
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-10-25 15:03:54 +0200

    Cleaned up AudioPlayer

--- src/brain/Interface/AudioPlayer.cpp
+++ src/brain/Interface/AudioPlayer.cpp
@@ -24,7 +24,6 @@ AudioPlayer::~AudioPlayer()
 
 void AudioPlayer::cycle()
 {
-  std::cout << "Frequency: " << audioRequest_->frequency << std::endl;
   if (!playbackData_->samples.empty())
   {
     audioInterface_.playbackAudioData(playbackData_->samples);
@@ -39,73 +38,14 @@ void AudioPlayer::cycle()
     if(lastFrameWritten_ < cycleStartTime){
       lastFrameWritten_ = cycleStartTime;
     }
-//    double timeToWrite = endTime_ - lastFrameWritten_;
 
     std::vector<float> samples;
     for(int i = 0; lastFrameWritten_ < endTime_; i++){
       double timeIisPlayed = lastFrameWritten_ + 1/44100.f; //s
       double value = sin(timeIisPlayed*frequency*2*M_PI);
       samples.push_back(value);
-//      std::cout << value << ";" << timeIisPlayed << std::endl;
       lastFrameWritten_ = timeIisPlayed; //s
     }
     audioInterface_.playbackAudioData(samples);
-
-
-    /*
-
-    //44100 Hz
-
-    double actualSampleStartTime = std::max(timeOfLastSample_, cycleStartTime); //s
-
-  /// ---------------|----------------|----------------|----------------|----------------|----------------|
-  /// =====SAMPLE=======I[------------][---]
-//    print("CycleStartTime: ", cycleStartTime, LogLevel::INFO);
-//    print("ActualSampleStartTime: ", actualSampleStartTime, LogLevel::INFO);
-
-    assert(actualSampleStartTime >= cycleStartTime);
-
-    double cycleTime = cycleInfo_->cycleTime; //s
-    double actualSampleTime = cycleTime - (actualSampleStartTime - cycleStartTime); //s
-
-    assert(actualSampleTime > 0);
-
-    float frequency = audioRequest_->frequency; //Hz
-
-
-//    int sampleCount = (int)std::ceil(((actualSampleTime + cycleTime*0.5)*44100.f)); //
-
-//    print("SampleCount: ", sampleCount, LogLevel::INFO);
-//    print("CycleTime: ", cycleTime, LogLevel::INFO);
-//    print("ActualSampleTime: ", actualSampleTime, LogLevel::INFO);
-//    print("LastIPlayed: ", timeOfLastSample_, LogLevel::INFO);
-//    print("=====", LogLevel::INFO);
-
-    double timeIisPlayed = 0; //s
-    double sampleEndTime = actualSampleStartTime + actualSampleTime;
-    double cycleEndTime = cycleStartTime + cycleTime;
-    assert(sampleEndTime >= cycleEndTime);
-    assert(sampleEndTime < cycleEndTime + cycleTime);
-
-    std::cout << 3 << ";" << actualSampleStartTime << std::endl;
-
-    for(int i = 0; timeIisPlayed < sampleEndTime; i++) {
-      timeIisPlayed = actualSampleStartTime + (double)i * 1/44100.f; //s
-      double value = sin(timeIisPlayed*frequency*2*M_PI);
-      samples.push_back(value);
-      std::cout << value << ";" << timeIisPlayed << std::endl;
-    }
-
-    timeOfLastSample_ = timeIisPlayed; //s
-    std::cout << 2 << ";" << timeOfLastSample_ << std::endl;
-
-//    print("Generated Samples: ", samples.size(), LogLevel::INFO);
-//    print("CycleTime: ", cycleTime, LogLevel::INFO);
-//    print("Periodduration: ", periodDuration, LogLevel::INFO);
-//    print("Frequency: ", frequency, LogLevel::INFO);
-//    print("Last Time: ", time, LogLevel::INFO);
-      audioInterface_.stopPlayback();
-      audioInterface_.playbackAudioData(samples);
-      audioInterface_.startPlayback();*/
   }
 }
