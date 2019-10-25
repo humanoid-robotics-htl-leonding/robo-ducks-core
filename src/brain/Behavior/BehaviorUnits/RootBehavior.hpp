369ab5741bac630fe7765a157046a0603bfb0c6d
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-10-25 15:02:32 +0200

    Reverted Root Behaviour

--- src/brain/Behavior/BehaviorUnits/RootBehavior.hpp
+++ src/brain/Behavior/BehaviorUnits/RootBehavior.hpp
@@ -3,21 +3,14 @@
 
 ActionCommand rootBehavior(const DataSet& d)
 {
-  //return ActionCommand::stand().combineAudio(ActionCommand::Audio::test());
+  const bool high = !d.bodyPose.footContact;
 
-
-  // If the NAO does not have foot contact it overrides the left LED with pink.
-  const bool front = d.buttonData.buttons[keys::sensor::SWITCH_HEAD_FRONT];
-  const bool middle = d.buttonData.buttons[keys::sensor::SWITCH_HEAD_MIDDLE];
-  const bool back = d.buttonData.buttons[keys::sensor::SWITCH_HEAD_REAR];
-
-  if(front){
-    return ActionCommand::dead().combineAudio(ActionCommand::Audio::audioC4());
-  }else if(middle){
-    return ActionCommand::dead().combineAudio(ActionCommand::Audio::audioC3());
-  }else if(back){
-    return ActionCommand::dead().combineAudio(ActionCommand::Audio::audioC5());
-  }else{
-    return ActionCommand::dead().combineAudio(ActionCommand::Audio::off());
+  if (d.gameControllerState.penalty == Penalty::NONE)
+  {
+    return high ? notPenalized(d).combineLeftLED(ActionCommand::LED::pink()) : notPenalized(d);
+  }
+  else
+  {
+    return high ? ActionCommand::penalized().combineLeftLED(ActionCommand::LED::pink()) : ActionCommand::penalized();
   }
 }
