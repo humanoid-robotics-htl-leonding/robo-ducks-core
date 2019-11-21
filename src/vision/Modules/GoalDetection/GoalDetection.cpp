c15c9a21616114189bd9db2121f4276db055c0a3
ZekReshi <florian.schwarcz@gmx.at>
2019-11-21 14:16:45 +0000

    WIP GoalDetection
    + scanning for possible goal points

--- src/vision/Modules/GoalDetection/GoalDetection.cpp
+++ src/vision/Modules/GoalDetection/GoalDetection.cpp
@@ -4,13 +4,82 @@
 
 GoalDetection::GoalDetection(const ModuleManagerInterface& manager)
   : Module(manager)
+  , imageData_(*this)
+  , cameraMatrix_(*this)
+  , filteredSegments_(*this)
   , goalData_(*this)
 {
 }
 
+Vector2f GoalDetection::getGradient(const Vector2i& p) const
+{
+		std::function<uint8_t(const YCbCr422&)> y1 = [](const YCbCr422& c) { return c.y1_; };
+		std::function<uint8_t(const YCbCr422&)> y2 = [](const YCbCr422& c) { return c.y2_; };
+		const uint8_t one = 1;
+		auto y = (p.x() & one) == 1 ? y2 : y1;
+		Vector2f gradient = Vector2f::Zero();
+		const Image422& im = imageData_->image422;
+		if (p.x() < 1 || p.y() < 1 || p.x() + 1 >= im.size.x() || p.y() + 1 >= im.size.y())
+		{
+				return gradient;
+		}
+		gradient.x() = y(im.at(p.y() + 1, p.x() - 1)) + 2 * y(im.at(p.y() + 1, p.x())) +
+									 y(im.at(p.y() + 1, p.x() + 1)) - y(im.at(p.y() - 1, p.x() - 1)) -
+									 2 * y(im.at(p.y() - 1, p.x())) - y(im.at(p.y() - 1, p.x() + 1));
+		gradient.y() = y(im.at(p.y() - 1, p.x() - 1)) + 2 * y(im.at(p.y(), p.x() - 1)) +
+									 y(im.at(p.y() + 1, p.x() - 1)) - y(im.at(p.y() - 1, p.x() + 1)) -
+									 2 * y(im.at(p.y(), p.x() + 1)) - y(im.at(p.y() + 1, p.x() + 1));
+		return gradient.normalized();
+}
+
+void GoalDetection::detectGoalPoints()
+{
+		goalPoints_.clear();
+		Vector2f g1, g2;
+		auto shift = [](int c) { return c >> 1; };
+		for (const auto& segment : filteredSegments_->horizontal)
+		{
+				if (segment->startEdgeType != EdgeType::RISING || segment->endEdgeType != EdgeType::FALLING)
+				{
+						continue;
+				}
+				g1 = getGradient(segment->start);
+				g2 = getGradient(segment->end);
+				if (g1.dot(g2) > -0.95)
+				{
+						continue;
+				}
+				goalPoints_.push_back((segment->start + segment->end).unaryExpr(shift));
+		}
+}
+
 void GoalDetection::cycle()
 {
-  // Chronometer time(debug(), mount_ + ".cycle_time");
-  // blank module
-  goalData_->valid = false;
+		if (!filteredSegments_->valid)
+		{
+				return;
+		}
+		{
+				Chronometer time(debug(), mount_ + "." + imageData_->identification + "_cycle_time");
+				detectGoalPoints();
+				debugGoalPoints_ = goalPoints_;
+				//ransacHandler();
+				//createGoalData();
+		}
+		sendImagesForDebug();
+		goalData_->valid = false;
+}
+
+void GoalDetection::sendImagesForDebug()
+{
+		auto mount = mount_ + "." + imageData_->identification + "_image_goals";
+		if (debug().isSubscribed(mount))
+		{
+				Image image(imageData_->image422.to444Image());
+				for (const auto& point : debugGoalPoints_)
+				{
+						image.circle(Image422::get444From422Vector(point), 2, Color::RED);
+				}
+				debug().sendImage(mount_ + "." + imageData_->identification + "_image_goals", image);
+		}
 }
--- src/vision/Modules/GoalDetection/GoalDetection.hpp
+++ src/vision/Modules/GoalDetection/GoalDetection.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
-#include "Data/GoalData.hpp"
 #include "Framework/Module.hpp"
+
+#include "Data/CameraMatrix.hpp"
+#include "Data/FilteredSegments.hpp"
+#include "Data/ImageData.hpp"
+#include "Data/GoalData.hpp"
 
 class Brain;
 
@@ -13,5 +17,32 @@ public:
   void cycle();
 
 private:
+	/**
+ 	 * @brief getGradient calculates the normalized gradient in the y channel
+ 	 * @param p a point in pixel coordinates at which the gradient is computed
+ 	 * @return the gradient
+ 	 */
+	Vector2f getGradient(const Vector2i& p) const;
+  /**
+   * @brief detectGoalPoints uses the scanline segments and detects points which could belong to a
+   * goal post
+   */
+  void detectGoalPoints();
+  /**
+	 * @brief sendImagesForDebug send debug information
+	 */
+	void sendImagesForDebug();
+
+  /// a reference to the image
+	const Dependency<ImageData> imageData_;
+	/// a reference to the camera matrix
+	const Dependency<CameraMatrix> cameraMatrix_;
+  /// a reference to the filtered segments
+	const Dependency<FilteredSegments> filteredSegments_;
+	/// the detected goal posts for other modules
   Production<GoalData> goalData_;
+  /// goal points for debug purposes
+	VecVector2i debugGoalPoints_;
+  /// candidate points on lines
+	VecVector2i goalPoints_;
 };
