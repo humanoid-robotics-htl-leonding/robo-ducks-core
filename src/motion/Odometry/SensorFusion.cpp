aa3992e3634a94fcb9b90e45d357069c277b28ff
Maximilian Wahl <mexx.wale@gmail.com>
2019-12-05 15:28:01 +0100

    potentially fixed hulks condition error

--- src/motion/Odometry/SensorFusion.cpp
+++ src/motion/Odometry/SensorFusion.cpp
@@ -25,13 +25,13 @@ void SensorFusion::update(const Vector3f& extGyro, const Vector3f& extAccel)
   Vector3d eigenExtGyro(extGyro.x(), extGyro.y(), -extGyro.z());
   Vector3d eigenExtAccel(-extAccel.x(), +extAccel.y(), -extAccel.z());
 
-  if (!initialized_ && eigenExtAccel.norm() >= 1.0)
+  if (!initialized_ && eigenExtAccel.norm() >= 8.0f)
   {
     calculateOrientation(eigenExtGyro, eigenExtAccel);
     initialized_ = true;
     return;
   }
-  else if (!initialized_ && eigenExtAccel.norm() < 1.0)
+  else if (!initialized_ && eigenExtAccel.norm() < 8.0f)
   {
     // Calculating the orientation of the nao while falling (low gravity)
     // would lead to big errors anyway
