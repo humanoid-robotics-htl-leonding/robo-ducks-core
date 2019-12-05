811d3f282eb8cfc41ce8b85b1f36612c1bb90fc8
Maximilian Wahl <mexx.wale@gmail.com>
2019-12-05 11:44:05 +0100

    finished reset and to and fromValue

--- src/tuhhsdk/Data/LEDRequest.hpp
+++ src/tuhhsdk/Data/LEDRequest.hpp
@@ -73,6 +73,23 @@ public:
       rightEyeR = 0.f;
       rightEyeG = 0.f;
       rightEyeB = 0.f;
+
+
+      leftEarMode = EarMode::OFF;
+      leftEarProgress = 0;
+      leftEarPulseSpeed = 0;
+      leftEarBrightness = 0;
+
+      rightEarMode = EarMode::OFF;
+      rightEarProgress = 0;
+      rightEarPulseSpeed = 0;
+      rightEarBrightness = 0;
+
+
+      chestMode = ChestMode::OFF;
+      chestR = 0;
+      chestG = 0;
+      chestB = 0;
     }
 
     void toValue(Uni::Value& value) const override
@@ -82,10 +99,28 @@ public:
         value["leftEyeR"] << leftEyeR;
         value["leftEyeG"] << leftEyeG;
         value["leftEyeB"] << leftEyeB;
+
         value["rightEyeMode"] << static_cast<int>(rightEyeMode);
         value["rightEyeR"] << rightEyeR;
         value["rightEyeG"] << rightEyeG;
         value["rightEyeB"] << rightEyeB;
+
+
+        value["leftEarMode"] << static_cast<int>(leftEarMode);
+        value["leftEarProgress"] << leftEarProgress;
+        value["leftEarPulseSpeed"] << leftEarPulseSpeed;
+        value["leftEarBrightness"] << leftEarBrightness;
+
+        value["rightEarMode"] << static_cast<int>(rightEarMode);
+        value["rightEarProgress"] << rightEarProgress;
+        value["rightEarPulseSpeed"] << rightEarPulseSpeed;
+        value["rightEarBrightness"] << rightEarBrightness;
+
+
+        value["chestMode"] << static_cast<int>(chestMode);
+        value["chestR"] << chestR;
+        value["chestG"] << chestG;
+        value["chestB"] << chestB;
     }
 
     void fromValue(const Uni::Value& value) override
@@ -94,10 +129,28 @@ public:
         value["leftEyeR"] >> leftEyeR;
         value["leftEyeG"] >> leftEyeG;
         value["leftEyeB"] >> leftEyeB;
+
         rightEyeMode = static_cast<EyeMode>(value["rightEyeMode"].asInt32()) ;
         value["rightEyeR"] >> rightEyeR;
         value["rightEyeG"] >> rightEyeG;
         value["rightEyeB"] >> rightEyeB;
+
+
+        leftEarMode = static_cast<EarMode >(value["leftEarMode"].asInt32()) ;
+        leftEarProgress = static_cast<uint8_t>(value["leftEarProgress"].asInt32());
+        leftEarPulseSpeed = static_cast<uint8_t >(value["leftEarPulseSpeed"].asInt32());
+        value["leftEarBrightness"] >> leftEarBrightness;
+
+        rightEarMode = static_cast<EarMode >(value["rightEarMode"].asInt32()) ;
+        rightEarProgress = static_cast<uint8_t>(value["rightEarProgress"].asInt32());
+        rightEarPulseSpeed = static_cast<uint8_t >(value["rightEarPulseSpeed"].asInt32());
+        value["rightEarBrightness"] >> rightEarBrightness;
+
+
+        chestMode = static_cast<ChestMode >(value["chestMode"].asInt32());
+        value["chestR"] >> chestR;
+        value["chestG"] >> chestG;
+        value["chestB"] >> chestB;
     }
 };
 
