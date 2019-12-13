74afccfcefecebe6e0b876d3dbdf59fc50fc13b6
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2019-12-13 17:36:08 +0100

    Finished Compile Script

--- scripts/midaslib/commands/compile.py
+++ scripts/midaslib/commands/compile.py
@@ -4,12 +4,11 @@ from argparse import ArgumentParser
 
 from midaslib.command import Command, command
 from midaslib.commonparsers import parse_address
-from midaslib.compiler import Targets, BuildTypes
+from midaslib.compiler import Targets, BuildTypes, Compiler
 
 
 @command("compile", "c")
 class CompileCommand(Command):
-    @parse_address
     def define_parser(self, parser: ArgumentParser):
         parser.add_argument("build", choices=BuildTypes.keys(), help="Build type")
         parser.add_argument("target", choices=Targets.keys(), help="Compile target")
@@ -18,7 +17,9 @@ class CompileCommand(Command):
     def execute(self, args):
         build_type = BuildTypes[args.build]
         target = Targets[args.target]
-        logging.info(f"Compiling: {build_type.name} {target.name}")
+        logging.info(f"Compiling '{build_type.name}' for '{target.name}'")
+        compiler = Compiler(build_type, target)
+        compiler.compile()
 
 
 if __name__ == '__main__':
--- scripts/midaslib/compiler.py
+++ scripts/midaslib/compiler.py
@@ -1,44 +1,55 @@
+import logging
+import os
+import subprocess
 from enum import Enum
 
 
 class CompileTarget:
-    name = "Undefined"
-    id = "undefined"
+    name = None
+    id = None
+    foldername = None
 
 
 class Nao5CompileTarget(CompileTarget):
     name = "Nao V5"
     id = "nao5"
+    foldername = "nao5"
 
 
 class Nao6CompileTarget(CompileTarget):
     name = "Nao V6"
     id = "nao6"
+    foldername = "nao6"
 
 
 class SimrobotCompileTarget(CompileTarget):
     name = "Simrobot"
     id = "simrobot"
+    foldername = "simrobot"
 
 
 class BuildType:
-    name = "Undefined"
-    id = "undefined"
+    name = None
+    id = None
+    foldername = None
 
 
 class DebugBuildType(BuildType):
     name = "Debug"
     id = "debug"
+    foldername = "Debug"
 
 
 class DevelopBuildType(BuildType):
     name = "Develop"
     id = "develop"
+    foldername = "Develop"
 
 
 class ReleaseBuildType(BuildType):
     name = "Release"
     id = "release"
+    foldername = "Release"
 
 
 Targets = {
@@ -53,6 +64,20 @@ BuildTypes = {
     ReleaseBuildType.id: ReleaseBuildType
 }
 
+project_root_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../.."))
+
 
 class Compiler:
-    pass
+    def __init__(self, build: BuildType, target: CompileTarget):
+        self.build_type = build
+        self.target = target
+        self.build_dir = os.path.join(project_root_dir, "build", self.target.foldername, self.build_type.foldername)
+
+    def compile(self):
+        cmake_cache_path = os.path.join(self.build_dir, "CMakeCache.txt")
+        if not os.path.exists(cmake_cache_path):
+            raise RuntimeError("Please execute setup before compile. (Missing CMakeCache.txt for target)")
+
+        used_cpus = os.cpu_count() - 1
+        logging.info(f"Using {used_cpus} Threads (-j{used_cpus})")
+        subprocess.run(["make", f"-j{used_cpus}"], cwd=self.build_dir)
