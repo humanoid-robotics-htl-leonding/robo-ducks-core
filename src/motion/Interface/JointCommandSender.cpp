83131c51017df1f0328bdcedc6b9d9395c8ecb7c
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2020-01-07 12:30:10 +0100

    Possible fix for JointCommandSender

--- src/motion/Interface/JointCommandSender.cpp
+++ src/motion/Interface/JointCommandSender.cpp
@@ -142,6 +142,9 @@ void JointCommandSender::cycle() {
             motionState_->rightArmMotion = MotionRequest::ArmMotion::BODY;
             motionState_->headMotion = MotionRequest::HeadMotion::BODY;
 
+
+		}
+
         // The head motion can be trusted that it only wants to send when it is allowed to.
         if (motionActivation_->headMotionActivation > 0.f) {
             for (unsigned int i = 0; i < JOINTS_HEAD::HEAD_MAX; i++) {
@@ -203,5 +206,4 @@ void JointCommandSender::cycle() {
 #endif
         robotInterface().setJointAngles(calibratedAngles_);
         robotInterface().setJointStiffnesses(stiffnesses_);
-    }
 }
