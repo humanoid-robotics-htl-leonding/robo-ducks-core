b777fc85883bc5e979a9311a3abec5900a060538
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2020-01-21 10:35:52 +0100

    Begun DataSetProvider

---
+++ src/brain/Behavior/DataSetProvider.hpp
@@ -0,0 +1,15 @@
+//
+// Created by obyoxar on 21/01/2020.
+//
+
+#ifndef DATASETPROVIDER_HPP
+#define DATASETPROVIDER_HPP
+
+
+class DataSetProvider: public Module<DataSetProvider, Brain>
+{
+
+};
+
+
+#endif //DATASETPROVIDER_HPP
