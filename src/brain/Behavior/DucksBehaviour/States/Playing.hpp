d7cd4bc0c791476490ddebcb59e84b6a39a0ba0b
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2020-01-28 16:57:43 +0100

    Added LookAtBall

--- src/brain/Behavior/DucksBehaviour/States/Playing.hpp
+++ src/brain/Behavior/DucksBehaviour/States/Playing.hpp
@@ -4,11 +4,16 @@ ActionCommand playing(const DuckDataSet &d)
 {
 //	auto command = roles(d);
 	auto command = ActionCommand::stand();
-	if(d.ballState.confident) {
-		auto ballPos = d.ballState.position;
+	if (d.ballState.age < 1.0 && d.ballState.confident) {
+		auto ballPos = d.ballState.position + d.ballState.velocity*d.cycleInfo.cycleTime;
 		command.combineHead(ActionCommand::Head::lookAt(
 			Vector3f(ballPos.x(), ballPos.y(), 0), 4, 4
-			));
+		)).combineLeftLED(ActionCommand::EyeLED::red());
+	}else if(d.teamBallModel.found && d.teamBallModel.insideField){
+		auto ballPos = d.robotPosition.fieldToRobot(d.teamBallModel.position + d.teamBallModel.velocity*d.cycleInfo.cycleTime);
+		command.combineHead(ActionCommand::Head::lookAt(
+			Vector3f(ballPos.x(), ballPos.y(), 0), 4, 4
+		)).combineLeftLED(ActionCommand::EyeLED::pink());
 	}
 	return command;
 }
