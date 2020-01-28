cc8349dd92db179f6388fa4b14866b58ab6eb8db
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2020-01-28 15:42:54 +0100

    Added most basic look at ball

--- src/brain/Behavior/DucksBehaviour/States/Playing.hpp
+++ src/brain/Behavior/DucksBehaviour/States/Playing.hpp
@@ -2,5 +2,13 @@
 
 ActionCommand playing(const DuckDataSet &d)
 {
-	return roles(d);
+//	auto command = roles(d);
+	auto command = ActionCommand::stand();
+	if(d.ballState.confident) {
+		auto ballPos = d.ballState.position;
+		command.combineHead(ActionCommand::Head::lookAt(
+			Vector3f(ballPos.x(), ballPos.y(), 0), 4, 4
+			));
+	}
+	return command;
 }
