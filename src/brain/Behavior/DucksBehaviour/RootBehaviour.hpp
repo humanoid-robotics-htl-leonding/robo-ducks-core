b06f60e71c462dc21984744aa9aecebac50b4a56
Maximilian Wahl <mexx.wale@gmail.com>
2020-02-12 13:25:10 +0100

    added medium kick

--- src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
+++ src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
@@ -7,7 +7,7 @@ ActionCommand rootBehavior(const DuckDataSet &d)
 {
 	if(d.cycleInfo.startTime-d.buttonData.lastChestButtonSinglePress<50){
           Vector2f source =Vector2f(0.2,0.06);
-          Vector2f target =Vector2f(3.7,0.06);
+          Vector2f target =Vector2f(3.1,0.06);
           return ActionCommand::kick(source,target,KickType::FORWARD);
     }
 	return ActionCommand::stand();
