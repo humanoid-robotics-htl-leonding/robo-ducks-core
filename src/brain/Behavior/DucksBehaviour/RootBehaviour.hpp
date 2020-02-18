1bf87bcaae94ad0811b593b084f79260ca359048
Maximilian Wahl <mexx.wale@gmail.com>
2020-02-18 14:22:32 +0100

    wip on improved goalkeeper-motions

--- src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
+++ src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
@@ -5,6 +5,11 @@
 
 DucksActionCommand rootBehavior(const DuckDataSet &d)
 {
+    //TODO check all motion files if they still work
+    //TODO jumpingCatch extend second arm
+    //TODO stationaryCatch find Foot Balance
+    //TODO buttcatch check if penetrable
+    //TODO flip fixed jumping and stationaryCatch
     if(d.cycleInfo.startTime-d.buttonData.lastChestButtonSinglePress <50){
         return DucksActionCommand::keeper(MotionKeeper::MK_TAKE_LEFT);
     }
