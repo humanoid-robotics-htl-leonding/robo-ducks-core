486275827e82efcbe34ed87e244ea8fe54060760
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2020-02-27 14:33:48 +0100

    Tested Keeper

--- src/brain/Behavior/DucksBehaviour/Utils/KickBall.hpp
+++ src/brain/Behavior/DucksBehaviour/Utils/KickBall.hpp
@@ -13,13 +13,20 @@ DucksActionCommand kick(const DuckDataSet& d, const Vector2f& targetPos){
 	float orientation = std::atan2(-goalToBall.y(), -goalToBall.x());
 
 	Pose ballCoordinateSystem = Pose(d.teamBallModel.position, orientation);
-	Pose robotPoseRelativeToBall = Pose(Vector2f(-0.15, 0.04), 0);
+	Pose robotPoseRelativeToBallR = Pose(Vector2f(-0.17, 0.06), 0);
+	Pose robotPoseRelativeToBallL = Pose(Vector2f(-0.17, -0.06), 0);
+
+	float distToR = (d.robotPosition.pose.position - robotPoseRelativeToBallR.position).norm();
+	float distToL = (d.robotPosition.pose.position - robotPoseRelativeToBallL.position).norm();
+
+	//TODO This does not seem to work. Please investigate.
+	Pose robotPoseRelativeToBall = distToR < distToL ? robotPoseRelativeToBallR : robotPoseRelativeToBallL;
 
 	Pose globalTargetRobotPosition = ballCoordinateSystem * robotPoseRelativeToBall;
 
 	Pose currentRobotPosition = d.robotPosition.pose;
 
-	if(currentRobotPosition.isNear(globalTargetRobotPosition)) {
+	if(currentRobotPosition.isNear(globalTargetRobotPosition, 0.045)) {
 		return DucksActionCommand::kick(d.robotPosition.fieldToRobot(d.teamBallModel.position),
 										d.robotPosition.fieldToRobot(targetPos));
 	}else{
