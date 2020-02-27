02643756bc330dd70263b980b7929c0af6810a00
Maximilian Wahl <mexx.wale@gmail.com>
2020-02-27 08:30:55 +0100

    fixed root behaviour

--- src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
+++ src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
@@ -5,10 +5,6 @@
 
 ActionCommand rootBehavior(const DuckDataSet &d)
 {
-    if(d.cycleInfo.startTime-d.buttonData.lastChestButtonSinglePress<50){
-        Vector2f source =Vector2f(0.2,0.06);
-        Vector2f target =Vector2f(3.4,1);
-        return ActionCommand::kick(source,target);
-    }
-    return ActionCommand::stand();
+    DucksActionCommand state = chooseState(d);
+    return state;
 }
