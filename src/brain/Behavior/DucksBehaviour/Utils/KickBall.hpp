28ffaf0eda4e38a69f54c1811b8b4a848a53b340
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2020-03-02 11:22:56 +0100

    Added EarLed to KickBall

--- src/brain/Behavior/DucksBehaviour/Utils/KickBall.hpp
+++ src/brain/Behavior/DucksBehaviour/Utils/KickBall.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <Behavior/DucksDataSet.hpp>
 /**
  * Automatically walks behind ball and tries to kick into the desired Position
  * @author Simon Holzapfel
@@ -14,7 +13,7 @@ DucksActionCommand kick(const DucksDataSet& d, const Vector2f& targetPos){
 	Vector2f goalToBall = d.teamBallModel.position - targetPos;
 	float orientation = std::atan2(-goalToBall.y(), -goalToBall.x());
 
-	Pose ballCoordinateSystem = Pose(d.ballState.position, orientation);
+	Pose ballCoordinateSystem = Pose(d.teamBallModel.position, orientation);
 	Pose robotPoseRelativeToBallR = Pose(Vector2f(-0.17, 0.06), 0);
 	Pose robotPoseRelativeToBallL = Pose(Vector2f(-0.17, -0.06), 0);
 
@@ -29,12 +28,12 @@ DucksActionCommand kick(const DucksDataSet& d, const Vector2f& targetPos){
 	Pose currentRobotPosition = d.robotPosition.pose;
 
 	if(currentRobotPosition.isNear(globalTargetRobotPosition, 0.045)) {
-		return DucksActionCommand::kick(d.robotPosition.fieldToRobot(d.ballState.position),
-										d.robotPosition.fieldToRobot(targetPos));
+		return DucksActionCommand::kick(d.robotPosition.fieldToRobot(d.teamBallModel.position),
+										d.robotPosition.fieldToRobot(targetPos)).combineLeftEarLED(DucksActionCommand::EarLED::brightness(1.0));
 	}else{
 		return DucksActionCommand::walk(
 			d.robotPosition.fieldToRobot(globalTargetRobotPosition),
-			WalkMode::WALK_BEHIND_BALL);
+			WalkMode::WALK_BEHIND_BALL).combineLeftEarLED(DucksActionCommand::EarLED::loading());
 	}
 
 }
