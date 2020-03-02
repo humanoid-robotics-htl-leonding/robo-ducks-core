12e933a03de80fef639f30d2360af7b96a9b1bc7
Maximilian Wahl <mexx.wale@gmail.com>
2020-03-02 17:32:20 +0100

    reimplemented stationary catches - TODO standup motion

--- src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
+++ src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
@@ -10,7 +10,7 @@ DucksActionCommand rootBehavior(const DuckDataSet &d)
     //TODO stationaryCatch find Foot Balance
     //TODO flip fixed stationaryCatch
     if(d.cycleInfo.startTime -d.buttonData.lastChestButtonSinglePress <3000 &&(double)d.cycleInfo.startTime>5000){
-        return DucksActionCommand::keeper(MotionKeeper::MK_JUMP_RIGHT);
+        return DucksActionCommand::keeper(MotionKeeper::MK_TAKE_RIGHT);
     }
     return DucksActionCommand::stand();
 }
