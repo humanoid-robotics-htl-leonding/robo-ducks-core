d6ce488fd087e5f28a5f7e8922f20f338faf4cef
QuirinEcker <quirin.ecker@bajupa.com>
2020-03-04 13:52:54 +0100

    kick is now working

--- src/brain/Behavior/DucksStrikerActionProvider.cpp
+++ src/brain/Behavior/DucksStrikerActionProvider.cpp
@@ -29,38 +29,12 @@ void DucksStrikerActionProvider::cycle()
 
     if (robotPosition_->pose.position.x() >= 0) { // is in enemy half
         if (absoluteBallPosition.x() >= 0 && teamBallModel_->found) {
-            Vector2f ball = teamBallModel_->position;
             Vector2f goal = Vector2f(fieldDimensions_->fieldLength / 2, 0);
-            Vector2f goalToBall = ball - goal;
-            Vector2f goalToBallNormalized = goalToBall.normalized();
-            Vector2f ballToRobotDistanceX = goalToBallNormalized * 0.20;
-            Vector2f ballToGoal = -goalToBall;
-            Vector2f ballToRobotDistanceY = Vector2f(ballToGoal.y(), -ballToGoal.x()).normalized() * 0.07;
-            Vector2f kickPosition = ball + ballToRobotDistanceY + ballToRobotDistanceX;
-            Vector2f kickPositionToGoal = goal - kickPosition;
-            float kickPoseAngle = std::atan2(ballToGoal.y(), ballToGoal.x());
 
-            Rotation2Df rotationMatrix(-kickPoseAngle);
-            Vector2f rotatedTargetVector = rotationMatrix.toRotationMatrix() * kickPositionToGoal;
-
-
-            strikerAction_->kickPose = Pose(kickPosition, kickPoseAngle);
-
-            strikerAction_->kickPose = Pose(kickPosition, kickPoseAngle);
-            strikerAction_->action = DucksStrikerAction::Action::WALK_TO_POS;
-            float difference = (robotPosition_->pose.position - kickPosition).norm();
-            float rotationDifference = robotPosition_->pose.orientation - strikerAction_->kickPose.orientation;
-
-
-            if (difference <= 0.1 && rotationDifference <= 0.1 && ballState_->found) {
-                strikerAction_->action = DucksStrikerAction::Action::KICK_INTO_GOAL;
-                strikerAction_->kickType = DucksStrikerAction::KickType::KICK;
-                strikerAction_->kickable = BallUtils::Kickable::LEFT;
-                strikerAction_->target = rotatedTargetVector;
-
-                strikerAction_->target = rotatedTargetVector;
-
-            }
+            strikerAction_->kickType = DucksStrikerAction::KickType::KICK;
+            strikerAction_->kickable = BallUtils::LEFT;
+            strikerAction_->target = goal;
+            strikerAction_->action = DucksStrikerAction::Action::KICK_INTO_GOAL;
         }
         else if (isSurrounded()) {
             // TODO: shoots to the nearest teammate
