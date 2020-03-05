18de63e9464753be50498d7c2f5db05b55fe32b0
Maximilian Wahl <mexx.wale@gmail.com>
2020-03-05 15:27:37 +0100

    fixed formatting of new MotionFiles

--- src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
+++ src/brain/Behavior/DucksBehaviour/RootBehaviour.hpp
@@ -6,8 +6,6 @@
 
 DucksActionCommand rootBehavior(const DuckDataSet &d)
 {
-    if(d.cycleInfo.startTime -d.buttonData.lastChestButtonSinglePress <9000 &&(double)d.cycleInfo.startTime>5000 && d.buttonData.lastChestButtonSinglePress >0){
-        return DucksActionCommand::keeper(MotionKeeper::MK_TAKE_LEFT);
-    }
-    return DucksActionCommand::stand();
+    DucksActionCommand state = chooseState(d);
+    return state;
 }
